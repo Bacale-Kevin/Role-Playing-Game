eabf6bda335f1658bc317c279c877d7e2716da20
Bacale-Kevin <bacale86@gmail.com>
2023-06-26 16:01:37 +0100

    Organised application folder structure

---
+++ Role Playing Game API/Controllers/Character.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Role_Playing_Game_API.Models;
+
+namespace Role_Playing_Game_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+       
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetById(int id)
+        {
+
+            var character = characters.FirstOrDefault(character => character.Id == id);
+
+            if(character == null) 
+                return NotFound(id.ToString());
+
+            return Ok(character);
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character character)
+        {
+            characters.Add(character);
+            
+            return Ok(characters);
+        }
+    }
+}
---
+++ Role Playing Game API/Service/InterFaces/ICharacterService.cs
@@ -0,0 +1,11 @@
+﻿using Role_Playing_Game_API.Models;
+
+namespace Role_Playing_Game_API.Service.InterFaces
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+        Character GetCharacterById(int id);
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
