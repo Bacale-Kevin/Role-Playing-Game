78e492fac928955acb3bb9921bf7a4d6a923bd2b
Bacale-Kevin <bacale86@gmail.com>
2023-06-26 22:03:25 +0100

    feat: install entifty framework  packages and added dbContext

---
+++ Role Playing Game API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Role_Playing_Game_API.Models;
+
+namespace Role_Playing_Game_API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+        
+        public DbSet<Character> Characters { get; set; }
+    }
+}
